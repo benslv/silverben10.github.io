43cf133036cbee91c3b24935da23df44a7c2d7cf
Ben Silverman <bensilverman10@gmail.com>
2020-05-09 18:07:00 +0100

    Add colour variables and some filler content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 
 import Layout from "../components/layout";
+import Navbar from "../components/navbar";
 
 import "../styles/index.scss";
 
 const IndexPage = () => (
-    <Layout title="Home">
-        <h1>Hello, world!</h1>
-        <p>Welcome to my new website.</p>
-    </Layout>
+	<Layout title="Home">
+		<header>
+			<p>Hi, my name is</p>
+			<h1>Ben Silverman</h1>
+			<p>
+				I’m a second-year Computer Science student currently at the University of York with
+				a keen interest in web design and development.
+			</p>
+		</header>
+	</Layout>
 );
 
 export default IndexPage;
