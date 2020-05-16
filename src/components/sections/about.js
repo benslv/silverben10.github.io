34881e62951550e3a2bd95e4bd84c111cedd3b3c
Ben Silverman <bensilverman10@gmail.com>
2020-05-16 18:16:49 +0100

    Add About section

---
+++ src/components/sections/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+        <p>
+            Hi! I'm Ben, a second year Computer Science student at the University of York. I built
+            this website to showcase the work I've been doing recently and demonstrate my skills in
+            web development.
+        </p>
+        <p>
+            <strong>Outside of my studies,</strong> I usually spend my time programming, reading, or
+            playing in the University Jazz and Concert bands.
+        </p>
+    </div>
+);
+
+export default About;
