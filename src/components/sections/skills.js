7550f15c8efa1621d260368df755c3c16763ea5a
Ben Silverman <bensilverman10@gmail.com>
2020-05-16 18:16:56 +0100

    Add Skills section

---
+++ src/components/sections/skills.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Skills = () => (
+    <div>
+        <h2>Skills</h2>
+        <ul aria-label="Languages">
+            <li>JavaScript</li>
+            <li>Python</li>
+            <li>Java</li>
+            <li>HTML</li>
+            <li>CSS/Sass</li>
+        </ul>
+        <ul aria-label="Frameworks">
+            <li>React</li>
+            <li>Gatsby</li>
+            <li>Express</li>
+            <li>Node</li>
+            <li>Bootstrap</li>
+        </ul>
+        <ul aria-label="Tools">
+            <li>Bash/ZSH</li>
+            <li>Git & GitHub</li>
+            <li>Photoshop</li>
+            <li>Sony Vegas Pro</li>
+        </ul>
+    </div>
+);
+
+export default Skills;
