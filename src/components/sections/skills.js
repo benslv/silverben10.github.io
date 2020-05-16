44bcd3ba8dd2f21f8a00baf549725b9ab3becf7b
Ben Silverman <bensilverman10@gmail.com>
2020-05-16 20:52:41 +0100

    Convert Skills section to styled components

--- src/components/sections/skills.js
+++ src/components/sections/skills.js
@@ -1,30 +1,48 @@
 import React from "react";
+import styled from "styled-components";
 
-import "./skills.scss";
+const StyledUl = styled.ul`
+    list-style-type: none;
+    line-height: 150%;
+    padding-left: 0;
+
+    margin-top: 0;
+
+    &:before {
+        content: attr(aria-label);
+        font-weight: bold;
+    }
+
+    li:before {
+        content: "‣ ";
+        font-size: 18px;
+        color: red;
+    }
+`;
 
 const Skills = () => (
     <div>
         <h2>Skills</h2>
-        <ul aria-label="Languages">
+        <StyledUl aria-label="Languages">
             <li>JavaScript</li>
             <li>Python</li>
             <li>Java</li>
             <li>HTML</li>
             <li>CSS/Sass</li>
-        </ul>
-        <ul aria-label="Frameworks">
+        </StyledUl>
+        <StyledUl aria-label="Frameworks">
             <li>React</li>
             <li>Gatsby</li>
             <li>Express</li>
             <li>Node</li>
             <li>Bootstrap</li>
-        </ul>
-        <ul aria-label="Tools">
+        </StyledUl>
+        <StyledUl aria-label="Tools">
             <li>Bash/ZSH</li>
             <li>Git & GitHub</li>
             <li>Photoshop</li>
             <li>Sony Vegas Pro</li>
-        </ul>
+        </StyledUl>
     </div>
 );
 
