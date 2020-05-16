b16277f00881c8c524ba955544c2410797e49775
Ben Silverman <bensilverman10@gmail.com>
2020-05-16 18:17:22 +0100

    Add new sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,17 @@ import Layout from "../components/layout";
 
 // Import sections
 import Hero from "../components/sections/hero";
+import About from "../components/sections/about";
+import Skills from "../components/sections/skills";
 
 // Import styles
 import "../styles/index.scss";
 
 const IndexPage = () => (
     <Layout title="Home">
-        <Hero></Hero>
+        <Hero />
+        <About />
+        <Skills />
     </Layout>
 );
 
