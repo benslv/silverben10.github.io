c6633ca6e7ee98cd327fa35f8583b41517980773
Ben Silverman <bensilverman10@gmail.com>
2020-05-16 18:33:39 +0100

    Install styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,5 @@ module.exports = {
         description: `My new website. Built by me, from scratch, with Gatsby!`,
         author: `@bensilverman_`,
     },
-    plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`],
+    plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`, `gatsby-styled-components`],
 };
