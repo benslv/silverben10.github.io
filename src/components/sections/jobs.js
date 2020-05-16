bba5dc5abe921aa46c71064e4d49962ff5682da0
Ben Silverman <bensilverman10@gmail.com>
2020-05-16 20:53:03 +0100

    Add jobs config entry

---
+++ src/components/sections/jobs.js
@@ -0,0 +1,52 @@
+const jobs = [
+    {
+        name: "CyberSoc",
+        role: "Press & Publicity Officer",
+        startDate: "Mar 2019",
+        endDate: "Mar 2020",
+        link: "https://cybersoc.co.uk/",
+        responsibilities: [
+            "Assisted CyberSoc President in liaising with external stakeholders.",
+            "Wrote and disseminated communications to CyberSoc members and the wider student body.",
+        ],
+    },
+    {
+        name: "Rewind Gaming",
+        role: "Founder & Director of Operations",
+        startDate: "Jun 2016",
+        endDate: "Nov 2019",
+        link: "https://twitter.com/rewindrl",
+        responsibilities: [
+            "Negotiated partnerships and sponsorships with other companies.",
+            "Devised and ran Rewind Gaming's social media campaigns.",
+            "Managed a team of 30+ volunteer staff.",
+            "Coordinated weekly events.",
+            "Graphic Design Lead, responsible for entire Rewind Gaming branding.",
+        ],
+    },
+    {
+        name: "NCSC",
+        role: "CyberFirst Residential Course",
+        startDate: "Aug 2017",
+        endDate: "Sept 2017",
+        link: "https://www.ncsc.gov.uk/information/cyberfirst",
+        responsibilities: [
+            "Week-long residential course run by CyberFirst, a branch of GCHQ. Introduced to penetration testing, learning how to take over remote systems using Kali Linux and Metasploit and exploit common web vulnerabilities such as SQL injection and XSS. Gained experience using digital forensics tools such as Wireshark to analyse web traffic. Culminated in a 12-hour CTF-style event, gathering data using the techniques and concepts I learned.",
+        ],
+    },
+    {
+        name: "MintTwist",
+        role: "Front-end Web Development Intern",
+        startDate: "Aug 2016",
+        endDate: "Sept 2016",
+        link: "https://www.minttwist.com/",
+        responsibilities: [
+            "Developed website landing pages for clients.",
+            "Managed Git repositories for site data.",
+            "Error-checked and resolved bugs in pre-existing code.",
+            "Translated design concepts into HTML & CSS.",
+        ],
+    },
+];
+
+export default jobs;
