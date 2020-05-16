381b2edcb9a5335c219b1a517b26d953eaa67929
Ben Silverman <bensilverman10@gmail.com>
2020-05-16 18:36:25 +0100

    Correct gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,9 @@ module.exports = {
         description: `My new website. Built by me, from scratch, with Gatsby!`,
         author: `@bensilverman_`,
     },
-    plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`, `gatsby-styled-components`],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-styled-components`,
+    ],
 };
