1802914a69b589694cfe86e0dda07db75a5e220d
Ben Silverman <bensilverman10@gmail.com>
2020-05-23 10:52:29 +0100

    Remove unused variables import

--- src/components/button.js
+++ src/components/button.js
@@ -2,45 +2,43 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.a`
-    @import "../styles/variables";
+	background-image: linear-gradient(
+		90deg,
+		rgba(0, 0, 0, 0) 0%,
+		rgba(0, 0, 0, 0) 50%,
+		red 50%,
+		red 50%
+	);
 
-    background-image: linear-gradient(
-        90deg,
-        rgba(0, 0, 0, 0) 0%,
-        rgba(0, 0, 0, 0) 50%,
-        red 50%,
-        red 50%
-    );
+	background-size: 200%;
 
-    background-size: 200%;
+	border: 5px solid red;
+	color: red;
+	font-weight: 700;
 
-    border: 5px solid red;
-    color: red;
-    font-weight: 700;
+	display: inline-block;
+	margin: 0;
+	padding: 0.5em 1.2em;
 
-    display: inline-block;
-    margin: 0;
-    padding: 0.5em 1.2em;
+	text-align: center;
+	text-decoration: none !important;
+	transition: background-position 0.15s, ease-in-out all 0.175s;
 
-    text-align: center;
-    text-decoration: none !important;
-    transition: background-position 0.15s, ease-in-out all 0.175s;
-
-    &:hover {
-        color: white;
-        background-position: -100% 100%;
-    }
+	&:hover {
+		color: white;
+		background-position: -100% 100%;
+	}
 `;
 
 const Button = (props) => {
-    return (
-        <StyledButton
-            href={props.href}
-            target={props.target ? props.target : ""}
-            rel="noopener noreferrer">
-            {props.children}
-        </StyledButton>
-    );
+	return (
+		<StyledButton
+			href={props.href}
+			target={props.target ? props.target : ""}
+			rel="noopener noreferrer">
+			{props.children}
+		</StyledButton>
+	);
 };
 
 export default Button;
