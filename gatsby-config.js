b3cb34a29872902393c98c68f39e811e99a597fb
Ben Silverman <bensilverman10@gmail.com>
2020-07-10 15:59:29 +0100

    Initial new commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,34 @@
 module.exports = {
-    siteMetadata: {
-        title: `Ben Silverman`,
-        description: `My new website. Built by me, from scratch, with Gatsby!`,
-        author: `@bensilverman_`,
-    },
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-sass`,
-        `gatsby-plugin-styled-components`,
-    ],
+	siteMetadata: {
+		title: `Ben Silverman`,
+		description: `A personal site for me, Ben, to showcase the work I've done and the experience I have!`,
+		author: `@bensilverman_`,
+	},
+	plugins: [
+		`gatsby-plugin-react-helmet`,
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `images`,
+				path: `${__dirname}/src/images`,
+			},
+		},
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `gatsby-starter-default`,
+				short_name: `starter`,
+				start_url: `/`,
+				background_color: `#663399`,
+				theme_color: `#663399`,
+				display: `minimal-ui`,
+				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+			},
+		},
+		// this (optional) plugin enables Progressive Web App + Offline functionality
+		// To learn more, visit: https://gatsby.dev/offline
+		// `gatsby-plugin-offline`,
+	],
 };
