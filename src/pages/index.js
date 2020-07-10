d4d0705f314cdb153cdeb5ce856c277dca4dbb7f
Ben Silverman <bensilverman10@gmail.com>
2020-07-10 16:21:54 +0100

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/index.scss";
+import "../styles/raster/raster.grid.css";
+
+const IndexPage = () => (
+	<>
+		<h1>Hello, world!</h1>
+		<p>My name is Ben.</p>
+	</>
+);
+
+export default IndexPage;
