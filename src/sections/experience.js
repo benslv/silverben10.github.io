2be7cf87e29ff22ae64a751c88b00e4e09a6f95c
Ben Silverman <bensilverman10@gmail.com>
2020-07-11 16:45:52 +0100

    Add beginnings of Experience section

--- src/sections/experience.js
+++ src/sections/experience.js
@@ -1,10 +1,56 @@
 import React from "react";
+import styled from "styled-components";
 
 import Section from "../components/section";
 
+import { jobs } from "../content";
+
+import { colors, fontSizes, easing } from "../styles/theme";
+
+const StyledTabs = styled.div`
+	display: flex;
+	align-items: flex-start;
+	position: relative;
+`;
+
+const StyledTabList = styled.ul`
+	width: max-content;
+	padding: 0;
+	margin: 0;
+	list-style: none;
+`;
+
+const StyledTabButton = styled.button`
+	outline: none;
+	border: none;
+	background: none;
+	font-size: ${fontSizes.base};
+	padding: 0;
+	height: 50px;
+
+	color: ${colors.grey};
+
+	display: flex;
+	width: 100%;
+
+	transition: ${easing};
+
+	:hover {
+		color: ${colors.darkGrey};
+	}
+`;
+
 const Experience = () => (
 	<Section emoji="👥" title="Experience">
-		buh
+		<StyledTabs>
+			<StyledTabList>
+				{jobs.map(({ name }) => (
+					<StyledTabButton className="tab-links" onClick={`openContent(event, ${name}`}>
+						{name}
+					</StyledTabButton>
+				))}
+			</StyledTabList>
+		</StyledTabs>
 	</Section>
 );
 
