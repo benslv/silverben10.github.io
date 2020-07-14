336b5198521908e8c84e8497a110741a75183738
Ben Silverman <bensilverman10@gmail.com>
2020-07-14 11:05:47 +0100

    Update CyberSoc responsibilities

--- src/content.js
+++ src/content.js
@@ -20,10 +20,11 @@ module.exports = {
 			role: "Press & Publicity Officer",
 			startDate: "Mar 2019",
 			endDate: "Mar 2020",
-			link: "https://cybersoc.co.uk/",
+			url: "https://cybersoc.co.uk/",
 			responsibilities: [
 				"Assisted CyberSoc President in liaising with external stakeholders.",
 				"Wrote and disseminated communications to CyberSoc members and the wider student body.",
+				"Designed and built new society website from scratch, using React (and Gatsby).",
 			],
 		},
 		{
@@ -31,7 +32,7 @@ module.exports = {
 			role: "Founder & Director of Operations",
 			startDate: "Jun 2016",
 			endDate: "Nov 2019",
-			link: "https://twitter.com/rewindrl",
+			url: "https://twitter.com/rewindrl",
 			responsibilities: [
 				"Negotiated partnerships and sponsorships with other companies.",
 				"Devised and ran Rewind Gaming's social media campaigns.",
@@ -45,7 +46,7 @@ module.exports = {
 			role: "CyberFirst Residential Course",
 			startDate: "Aug 2017",
 			endDate: "Sept 2017",
-			link: "https://www.ncsc.gov.uk/information/cyberfirst",
+			url: "https://www.ncsc.gov.uk/information/cyberfirst",
 			responsibilities: [
 				"Week-long residential course run by CyberFirst, a branch of GCHQ. Introduced to penetration testing, learning how to take over remote systems using Kali Linux and Metasploit and exploit common web vulnerabilities such as SQL injection and XSS. Gained experience using digital forensics tools such as Wireshark to analyse web traffic. Culminated in a 12-hour CTF-style event, gathering data using the techniques and concepts I learned.",
 			],
@@ -55,7 +56,7 @@ module.exports = {
 			role: "Front-end Web Development Intern",
 			startDate: "Aug 2016",
 			endDate: "Sept 2016",
-			link: "https://www.minttwist.com/",
+			url: "https://www.minttwist.com/",
 			responsibilities: [
 				"Developed website landing pages for clients.",
 				"Managed Git repositories for site data.",
@@ -72,7 +73,7 @@ module.exports = {
 				"Website for CyberSoc, the University of York’s Cyber Security society. Designed and built from scratch, using GatsbyJS to leverage static site hosting with dynamic content.",
 			techs: ["React", "Express", "Gatsby", "HTML", "Sass"],
 			repo: "https://github.com/CyberSocYork/cybersocyork.github.io ",
-			link: "https://cybersoc.co.uk",
+			url: "https://cybersoc.co.uk",
 		},
 		{
 			name: "WikiCard Viewer",
@@ -80,7 +81,7 @@ module.exports = {
 				"A web app using ReactJS that allows users to search for a term on Wikipedia and have the results returned to them as a set of aesthetic cards.",
 			techs: ["React", "JavaScript", "HTML", "CSS"],
 			repo: "https://github.com/silverben10/wiki-card-viewer",
-			link: "https://bensilverman.co.uk/wiki-card-viewer/",
+			url: "https://bensilverman.co.uk/wiki-card-viewer/",
 		},
 	],
 };
